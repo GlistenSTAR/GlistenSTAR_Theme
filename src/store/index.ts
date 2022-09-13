450f71e636e54f0300229564e58a95b20fba3685
Glisten**STAR** <glistenstar00@gmail.com>
2022-09-13 19:58:48 +0200

    intergrate with redex-devtool chrome extension

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from 'redux-logger'
+
 import todoSlice from "./todo-slice";
 
 const store = configureStore({
     reducer: { todo: todoSlice.reducer },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
