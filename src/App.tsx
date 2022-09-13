74d24f17a46555129003c2c28b378ee1be7ba3e1
Glisten**STAR** <glistenstar00@gmail.com>
2022-09-13 21:13:58 +0200

    add the react-router-dom@6.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 
-import store from './store'
+import { Fragment } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Todo from './components/todo/Todo';
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+
+import store from './store'
 
 import './App.css';
-import Todo from './components/todo/Todo';
 
 function App() {
   return (
     <Provider store={store}>
-      <Todo></Todo>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/todo" element={<Todo />} />
+          </Routes>
+        </Fragment>
+      </Router>
     </Provider>
   );
 }
